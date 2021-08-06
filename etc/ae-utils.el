3f541445818e3cb917fb115a69c8bb0531c95037
JimMoen <LnJimMoen@outlook.com>
2021-08-06 20:53:08 +0800

    Add Rime Input Method.

---
+++ etc/ae-utils.el
@@ -0,0 +1,54 @@
+;;; ae-utils.el --- Useful Apps -*- lexical-binding: t -*-
+
+;; Copyright (C) 2021  JimMoen
+
+;; Author: JimMoen <LnJimMoen@outlook.com>
+;; Keywords: Utility Apps
+
+
+;; This file is not part of GNU Emacs
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
+;;; Commentary:
+
+;; Chinese Input Method.  Translate Apps
+;; Calendar Settings.
+;; And some other tools.
+
+;;; Code:
+
+;; Chinese input method
+;; rime (Melpa)
+(use-package rime
+  :custom
+  (default-input-method "rime")
+  :config
+  (setq rime-translate-keybindings
+        '("C-h" "C-g" "C-d" "C-k"
+          "C-f" "C-b" "C-n" "C-p" "C-a" "C-e" "C-v" "M-v" "<tab>" "S-<tab>" "C-`"
+          "<left>" "<right>" "<up>" "<down>" "<prior>" "<next>" "<delete>"))
+  (setq rime-user-data-dir "~/.local/share/fcitx5/rime")  ;; Use Fcitx5
+  (setq rime-show-candidate 'posframe)
+  (setq rime-posframe-properties
+        (list :background-color "#555555"
+              :foreground-color "#dcdcdc"
+              :font "sarasa mono sc-13"
+              :internal-border-width 4)))
+
+
+(provide 'ae-utils)
+
+;;; ae-utils.el ends here
