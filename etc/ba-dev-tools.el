1036f74618a77f129224bb5f96f7f4751cbeabe1
JimMoen <LnJimMoen@outlook.com>
2022-05-19 23:08:53 +0800

    feat(magit): quicklly split and open magit

--- etc/ba-dev-tools.el
+++ etc/ba-dev-tools.el
@@ -81,6 +81,10 @@ on the current line, if any."
                        (format "git commit --amend --author='%s'" author)
                      ""))
        arg)))
+  (defun my/split-then-magit ()
+    (interactive)
+    (switch-window-then-split-horizontally 0)
+    (magit-status))
   :general
   ("C-x g" 'magit-status
    "C-x G" 'magit-blame-addition)
