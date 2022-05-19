28c61b9dff9edfab95f804eb575b22a9f9676469
JimMoen <LnJimMoen@outlook.com>
2022-05-19 23:06:36 +0800

    feat(vue): vue language server

---
+++ etc/bc-dev-lang/lang-vue.el
@@ -0,0 +1,49 @@
+;;; lang-vue.el --- VUE support. -*- lexical-binding: t -*-
+
+;; Copyright (C) 2021  JimMoen
+
+;; Author: JimMoen <LnJimMoen@outlook.com>
+;; Keywords: VUE
+
+
+;; This file is not part of GNU Emacs
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
+;;; Commentary:
+
+;; Programming Language VUE support.
+
+;;; Code:
+
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 5))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
+(straight-use-package
+ '(lsp-volar :type git :host github :repo "jadestrong/lsp-volar"))
+
+(provide 'lang-vue)
+
+;;; lang-vue.el ends here
