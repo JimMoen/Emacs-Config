a5eeac1507eafe5b885d41ce6753069d3ae0d1d2
JimMoen <LnJimMoen@outlook.com>
2023-12-15 16:07:02 +0800

    feat(spell): flycheck-aspell for spell check
    see also:
    https://github.com/leotaku/flycheck-aspell/blob/master/README-FLYCHECK.md

--- etc/init-base.el
+++ etc/init-base.el
@@ -421,6 +421,36 @@
                                 "*Compile-Log*")))
 
 
+(use-package flycheck-aspell
+  :hook
+  (prog-mode . flyspell-prog-mode)
+  :config
+  ;; If you want to check TeX/LaTeX/ConTeXt buffers
+  (add-to-list 'flycheck-checkers 'tex-aspell-dynamic)
+  ;; If you want to check Markdown/GFM buffers
+  (add-to-list 'flycheck-checkers 'markdown-aspell-dynamic)
+  ;; If you want to check HTML buffers
+  (add-to-list 'flycheck-checkers 'html-aspell-dynamic)
+  ;; If you want to check XML/SGML buffers
+  (add-to-list 'flycheck-checkers 'xml-aspell-dynamic)
+  ;; If you want to check Nroff/Troff/Groff buffers
+  (add-to-list 'flycheck-checkers 'nroff-aspell-dynamic)
+  ;; If you want to check Texinfo buffers
+  (add-to-list 'flycheck-checkers 'texinfo-aspell-dynamic)
+  ;; If you want to check comments and strings for C-like languages
+  (add-to-list 'flycheck-checkers 'c-aspell-dynamic)
+  ;; If you want to check message buffers
+  (add-to-list 'flycheck-checkers 'mail-aspell-dynamic)
+
+  ;; Because Aspell does not support Org syntax, the user has
+  ;; to define a checker with the desired flags themselves.
+  ;; (flycheck-aspell-define-checker "erlang"
+  ;;   "Erlang" ("--add-filter" "url")
+  ;;   (erlang-mode))
+  ;; (add-to-list 'flycheck-checkers 'erlang-aspell-dynamic)
+  )
+
+
 (provide 'init-base)
 
 ;;; init-base.el ends here
