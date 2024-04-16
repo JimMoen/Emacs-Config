1a8726cdf6988ef8c3c889c217a8c4f3c6769afa
JimMoen <LnJimMoen@outlook.com>
2024-04-16 15:38:13 +0800

    feat: fzf integration

--- etc/init-base.el
+++ etc/init-base.el
@@ -136,6 +136,21 @@
   (("C-s"       . swiper)
    ("C-r"       . swiper)))
 
+(use-package fzf
+  :bind
+  ("C-c f" . fzf)
+  :config
+  (setq fzf/args "-x --color bw --print-query --margin=1,0 --no-hscroll"
+        fzf/executable "fzf"
+        fzf/git-grep-args "-i --line-number %s"
+        ;; command used for `fzf-grep-*` functions
+        ;; example usage for ripgrep:
+        fzf/grep-command "rg --no-heading -nH"
+        ;; fzf/grep-command "grep -nrH"
+        ;; If nil, the fzf buffer will appear at the top of the window
+        fzf/position-bottom t
+        fzf/window-height 15))
+
 ;; avoid call tramp and input path duplicated
 ;; sudo-edit (Melpa)
 (use-package sudo-edit
