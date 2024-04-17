796c26df47482c723f98be4032ab19461b0a80a6
JimMoen <LnJimMoen@outlook.com>
2024-04-17 11:51:32 +0800

    fix: cmake-mode as required package

--- etc/init-core.el
+++ etc/init-core.el
@@ -150,6 +150,7 @@
       go-mode python-mode
       php-mode js2-mode dart-mode jq-mode
       qml-mode
+      cmake-mode
       protobuf-ts-mode ;; proto3 only
       hcl-mode ;; for .hocon files
       typescript-mode ;; Vue && typescript && JavaScript
