950970a07f83a3f93a66fd079721c64da046dde4
JimMoen <LnJimMoen@outlook.com>
2025-09-10 10:46:07 +0800

    fix: increase ace window display index

--- etc/init-base.el
+++ etc/init-base.el
@@ -464,7 +464,7 @@ its value will be updated. If the key is not present, the entry will be added."
         aw-dispatch-always         nil
         aw-minibuffer-flag         t
         ;; aw-char-position           'top-left
-        aw-dispatch-when-more-than 3)
+        aw-dispatch-when-more-than 5)
   (setq aw-dispatch-alist
         '((?x aw-delete-window              "  Ace - Delete Window")
           (?m aw-swap-window                "  Ace - Swap Window")
